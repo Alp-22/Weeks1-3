3bb748b7fefbc95a563b025bdef3df0c63930012
SHERNET\perekm <perekm@sheridancollege.ca>
2025-01-07 17:57:52 -0500

    Added a square with moving script

---
+++ Assets/FirstScript.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    float speed = 0.02f;
+    //public Vector2 objectPos = transform.position;
+    public Vector3 worldPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            Vector2 objectPos = transform.position;
+        //Vector3 mousePos = Input.mousePosition;
+        /*Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 pos = Input.mousePosition;
+        //Mouse.current.position.ReadValue();
+        //pos.x += speed;
+        mouseWorldPosition.z = 0f;
+        transform.position = mouseWorldPosition;
+        print(pos);*/
+        if (Input.GetKey(KeyCode.W))
+        {
+            objectPos.y += speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            objectPos.y -= speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            objectPos.x -= speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            objectPos.x += speed;
+        }
+        transform.position = objectPos;
+        
+    }
+}
